8193058c0023d4e1f101c23cf8e01c8f21bef7f9
Kieran Everett <kieraneverett03@gmail.com>
2022-01-12 01:41:27 +0000

    Created a rolling average of solve times

--- main.py
+++ main.py
@@ -35,6 +35,26 @@ def getSolveTimes():
     
     return output
 
+def generateRollingAverage(solveTimes):
+    averageLength = 50
+
+    averaged = []
+    current = [i for i in range(1,averageLength+1)]
+    count = 0
+    looped = False
+    for i in solveTimes:
+        if count == averageLength:
+            count = 0
+            looped = True
+        current[count] = i
+        if looped == True:
+            averaged.append(sum(current)/len(current))
+        else:
+            averaged.append(30)
+        count += 1
+    return averaged
+        
+
 def graph(solveTimes):
 
     #longestSolve = math.ceil(max(solveTimes))
@@ -44,6 +64,7 @@ def graph(solveTimes):
     #    y.append(i)
 
     plt.plot(solveTimes)
+    plt.plot(generateRollingAverage(solveTimes))
 
     #solveTimes = np.asarray(solveTimes)
 
